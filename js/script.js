555e4f3b38238b398266feaa8c7f76c76a06f38e
mahmoud305 <61843121+mahmoud305@users.noreply.github.com>
2021-08-27 21:30:22 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ let aboutDistance= $("#About").offset().top;
        $(".nav-item a").css("color","black");
        $(".navbar-brand img").attr("src", "images/logo-dark.png");
        $("#myNav").css("padding","5px") ;
-
+       $("#upButton").fadeIn(1000);
     }
     else{
         $("#myNav").css("backgroundColor","transparent") ;
@@ -21,6 +21,43 @@ let aboutDistance= $("#About").offset().top;
         console.log($(".navbar-brand img").attr("src"));
         $(".navbar-brand img").attr("src", "images/logo-light.png");
         $("#myNav").css("padding","20px") ;
+        // $("#upButton").css("display","none");
+        $("#upButton").fadeOut(1000);
        }
  
  })
+
+ /* bt7ml mbt3mlsh scroll 3la tol ????????????????????????????*/ 
+$("#upButton").click(function(){
+    $("html,body").animate({scrollTop:0} , 1000);
+        // $("html,body").animate({scrollTop:0} ,2000);
+})
+
+
+$(".nav-link").click(function(e){
+    // alert("linkClicked");
+    let linkHref= $(e.target).attr("href");
+    let nextpostion= $(linkHref).offset().top;
+    $("html,body").animate({scrollTop:nextpostion} , 3000);    
+
+})
+
+$("#colorChangerIcon").click(function(){
+    let colorBoxWidth=$("#colorBox").innerWidth();
+    // alert(colorBoxWidth);
+    if($("#colorBoxContainer").css("left") == '0px'){   
+    $("#colorBoxContainer").animate({left:`-${colorBoxWidth}`},1000)
+}else
+    $("#colorBoxContainer").animate({left:`0px`},1000)
+})
+
+$(".colorItem").click(function () {
+    let newColor=$(this).css("backgroundColor");
+    $(".changeColor").css("color",newColor);
+  })
+
+
+  $(document).ready(function () {
+      $("#loadingScreen").fadeOut(1000,function(){  $("body").css("overflow","auto");});
+     
+  });
